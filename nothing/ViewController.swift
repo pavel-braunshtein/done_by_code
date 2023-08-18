21bb22adf3c613f531f8cc63bbd42958a0113bc1
pavelbraun333 <pavelbraun333@gmail.com>
2023-08-18 19:01:10 +0300

    Initial Commit

---
+++ nothing/ViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+import SwiftUI
+
+final class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        
+        let Label = UILabel()
+        Label.text = "Title"
+        Label.font = UIFont.systemFont(ofSize: 40)
+        Label.frame = CGRect(x: 160, y: 300, width: 200, height: 100)
+        view.addSubview(Label)
+        
+    }
+}
+
+struct MovieDetailsViewControllerRepresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        MovieDetailsViewController().view
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
+
+struct MovieDetailsViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailsViewControllerRepresentable()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
